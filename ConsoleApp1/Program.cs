b9199b47ffd93c2ac995ba0415200d67ff66e19b
Shirazbello <shirazbello@gmail.com>
2023-05-03 12:27:01 +0300

    Uniqueid

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentCassandra;
+using System.Net;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time Based Guid");
+            Guid TimeGuid1 = GuidGenerator.GenerateTimeBasedGuid();
+            Console.WriteLine(TimeGuid1.ToString());
+
+        }
+
+    }
+    
+
+}
